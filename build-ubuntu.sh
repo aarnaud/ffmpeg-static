6f6fb192f459615a17c64c7669ea18becd6f3fde
Anthony ARNAUD <perso@anthony-arnaud.fr>
2015-12-11 08:43:50 +0100

    Upgrade ffmpeg to 2.8.3

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install build-essential autoconf libtool curl tar cmake
+sudo apt-get install build-essential autoconf libtool curl tar cmake pkg-config
 
 ./build.sh
--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ cd $BUILD_DIR
 ../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
 ../fetchurl "http://zlib.net/zlib-1.2.8.tar.gz"
 ../fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
-../fetchurl "http://sourceforge.net/projects/libpng/files/libpng16/1.6.16/libpng-1.6.16.tar.gz"
+../fetchurl "http://sourceforge.net/projects/libpng/files/libpng16/1.6.19/libpng-1.6.19.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
@@ -53,8 +53,7 @@ cd $BUILD_DIR
 ../fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.3.tar.gz"
 ../fetchurl "http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz"
 ../fetchurl "https://github.com/mstorsjo/fdk-aac/archive/v0.1.3.tar.gz"
-../fetchurl "http://ffmpeg.org/releases/ffmpeg-2.5.3.tar.bz2"
-
+../fetchurl "http://ffmpeg.org/releases/ffmpeg-2.8.3.tar.bz2"
 
 
 echo "*** Building yasm ***"
